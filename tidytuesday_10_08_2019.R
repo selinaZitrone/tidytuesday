f27ccc63ef4ef2e2d74ff04a7b186d2eba67dfcb
Selina Baldauf <selina.baldauf@gmx.de>
2019-08-15 11:13:21 +0200

    Âfinal version bob ross Augus10 2019

--- tidytuesday_10_08_2019.R
+++ tidytuesday_10_08_2019.R
@@ -45,23 +45,25 @@ frame <- png::readPNG("images/bobRossFrameLandscape4.png")
 
 
 
-bob_ross_content %>% merge(bobRossCateg, by="variable") %>% 
+p1 <- bob_ross_content %>% merge(bobRossCateg, by="variable") %>% 
+  .[variable%in%c("tree","trees"), variable:="tree(s)"] %>% 
+  .[variable%in%c("mountain","mountains"),variable:="mountain(s)"] %>%  
   .[,sum(value),by=.(category,variable)] %>% 
   .[,variable:=gsub("_"," ",variable)] %>% 
   .[variable =="steve ross", variable:="Steve Ross"] %>% 
   .[variable == "diane_andre", variable := "Diane Andre"] %>% 
-  .[,angle:= 90 * sample(c(0, 1), 51, replace = TRUE, prob = c(60, 40))] %>% 
+  .[,angle:= 90 * sample(c(0, 1), nrow(.), replace = TRUE, prob = c(60, 40))] %>% 
   ggplot(aes(label = variable, size = V1, color=category, angle = angle))+
   geom_text_wordcloud()+
-  scale_size_area(max_size=20)+
+  scale_size_area(max_size=12)+
   theme_minimal()+
-  scale_color_viridis_d(option="inferno", direction = -1)+
+  scale_color_viridis_d(option="D", direction = -1)+
   annotation_custom(grid::rasterGrob(frame,
                     width = unit (.9, "npc"),
                     height = unit(.9, "npc")),
         -Inf, Inf, -Inf, Inf)+
-  theme(legend.position = "none",
-        panel.background = element_rect(fill = 'grey23'))
+  theme(legend.position = "none")#,
+        #panel.background = element_rect(fill = 'grey23'))
 
 #legend
 
@@ -73,11 +75,43 @@ bob_ross_content %>% merge(bobRossCateg, by="variable") %>%
   scale_color_viridis_d(option="viridis", direction = -1)+
   theme_void()+
   theme(legend.position = "none",
-        panel.background = element_rect(fill = 'grey23')
+        #panel.background = element_rect(fill = 'grey23')
         )
 
-bob_ross_content %>% merge(bobRossCateg, by="variable") %>% 
+#pie/donut chart content
+
+p2 <- bob_ross_content %>% merge(bobRossCateg, by="variable") %>% 
   .[,sum(value),by=.(category)] %>% 
   .[order(category)] %>% 
-  ggplot()
+  .[,prop := round((V1/(.[,sum(V1)]))*100,0)] %>% 
+  ggplot(aes(x=2, y = prop, fill = category))+
+  geom_bar(stat = "identity", color="white")+
+  coord_polar("y",start=0)+
+  #geom_text(aes(y= prop, label = prop), color = "white")+
+  scale_fill_viridis_d(option="viridis", direction = -1)+
+  theme_void()+
+  theme(legend.position = "left")+#,
+        #plot.background = element_rect(fill = 'grey23'),
+        #legend.text = element_text(color="white"))
+  xlim(0.5,2.5)
+  # theme(legend.position = "none",
+  #       panel.background = element_rect(fill = 'grey23')
+  # )
 
+#pie/donut chart frames
+
+bob_ross_frames[,.(not_framed)] %>% 
+  .[,.N, by=not_framed] %>% 
+  .[,prop := round((N/.[,sum(N)])*100,0)] %>% 
+  .[not_framed == 1,c("type","labelpos") :=.("no frame", prop/2)] %>% 
+  .[not_framed == 0,c("type","labelpos") :=.("frame", prop/2 + 87)] %>% 
+  ggplot(aes(x="", y=prop, fill = type))+
+  geom_bar(stat="identity", color = "white")+
+  geom_text(aes(x="",label=paste0(prop,"%"), y=labelpos), color ="white")+
+  theme_void()
+
+bob_ross_frames
+
+
+p3 <- ggpubr::ggarrange(p1,p2, widths = c(1.2, 0.8))
+ggsave("images/bobRossResult.pdf", p3, width = 20, height = 10, unit = "cm")
